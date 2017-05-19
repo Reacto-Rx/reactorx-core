5a0161e6460e9d94a446be0320b1a292066115c7
Filip Procházka <me@filipproch.cz>
2017-05-19 10:51:55 +0200

    working on fixing typebehaviorsubject

--- library/src/test/java/cz/filipproch/reactor/rx/TypeBehaviorSubjectTest.kt
+++ library/src/test/java/cz/filipproch/reactor/rx/TypeBehaviorSubjectTest.kt
@@ -1,12 +1,49 @@
 package cz.filipproch.reactor.rx
 
-import org.junit.Assert.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
 
-/**
- * TODO: add description
-
- * @author Filip Prochazka (@filipproch)
- */
 class TypeBehaviorSubjectTest {
+
+    private lateinit var subject: TypeBehaviorSubject<TypedObject>
+
+    @Before
+    fun setupInstance() {
+        subject = TypeBehaviorSubject.create()
+    }
+
+    @Test
+    fun testObjectBeingPersisted() {
+        val test = TestObject()
+        subject.onNext(test)
+
+        val observable = subject.asObservable()
+        val result = observable.take(1).blockingFirst()
+
+        assertThat(test).isEqualTo(result)
+    }
+
+    @Test
+    fun testRememberTwoDifferentTypes() {
+
+    }
+
+    @Test
+    fun testEmitCorrectItemWhenThereAreMultipleTypes() {
+
+    }
+
+    class TestObject : TypedObject {
+        override fun getType(): Class<*> {
+            return TestObject::class.java
+        }
+    }
+
+    class TestObject2 : TypedObject {
+        override fun getType(): Class<*> {
+            return TestObject2::class.java
+        }
+    }
 
 }
